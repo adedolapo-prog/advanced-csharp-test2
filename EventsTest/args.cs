df6fdab699827ae1d083255bf5f8cfa9dc10cfde
adedolapo-prog <shayne.doly94@gmail.com>
2024-04-23 18:08:48 -0700

    Add project files.

---
+++ EventsTest/args.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsTest
+{
+    public class args: EventArgs
+    {
+        public string Name { get; set; }
+
+        public args(string name)
+        {
+            Name = name;
+        }
+    }
+}
